3982aabd0d497c6fd2e761f2f03f803e0fc6c9ab
TongWei.Chen <15620646321@163.com>
2016-12-23 12:10:36 +0800

    提交

---
+++ shiro-example/shiro-example-chapter3/src/main/java/com/github/GeekerDream/shiro_example_chapter3/App.java
@@ -0,0 +1,13 @@
+package com.github.GeekerDream.shiro_example_chapter3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
